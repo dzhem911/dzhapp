2097e61a4e477782848a784020c9adcc90578581
dzhem911 <dzhemal911@gmail.com>
2022-09-14 01:28:17 +0300

    async chunks with lazy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,20 @@ import {Link, Route, Routes } from 'react-router-dom';
 import '../index.sass'
 import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "../pages/MainPage/MainPage.async";
+import {Suspense} from "react";
 
 const App = () => {
   return (
     <div className='app'>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Routes>
-        <Route path={'/about'} element={<AboutPageAsync/>} />
-        <Route path={'/'} element={<MainPageAsync/>} />
-      </Routes>
+      {/*ToDo create css loader*/}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            <Route path={'/about'} element={<AboutPageAsync/>} />
+            <Route path={'/'} element={<MainPageAsync/>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
